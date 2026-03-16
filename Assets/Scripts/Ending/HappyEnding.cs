ebb806050270c28fb8f3f735af9d92908bf2a719
yoosoo <niceball1129@gmail.com>
2026-03-16 21:33:38 +0900

    cc

---
+++ Assets/Scripts/Ending/HappyEnding.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HappyEnding : MonoBehaviour
+{
+    [SerializeField]
+    Button NextBtn;
+    [SerializeField]
+    Button SkipBtn;
+
+    string[] TutorialsLines = 
+    {};
+}
